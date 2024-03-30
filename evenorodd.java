de42b8e47d68dccb994ad787d0c5007106419566
vijaykumargup <106105269+vijaykumargup@users.noreply.github.com>
2024-03-30 17:18:55 +0530

    Add files via upload

---
+++ evenorodd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class evenorodd {
+    public static void main(String args[]){
+        Scanner vi=new Scanner(System.in);
+        System.out.println("enter any number");
+        int a=vi.nextInt();
+        if(a%2==0){
+            System.out.println("Its even number");
+        }
+        else{
+            System.out.println("its odd number");
+        }
+
+
+    }
+    
+}
